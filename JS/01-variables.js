a6a00d440f7a6be16990734c29bfb6420736ed47
Marcos Lautaro Molina <marcoslautaromolina@gmail.com>
2025-08-26 16:44:36 -0300

    agregamos un ejemplo de comentarios, como msotrar mensajes y un alert

---
+++ JS/01-variables.js
@@ -0,0 +1,20 @@
+// Esto es un comentario de una linea
+
+/*
+Esto es un comentario 
+de multiple lineas
+*/
+
+// Como mandar mensajes
+console.log("Hola mundo");
+console.info("Hola mundo");
+console.warn("Hola mundo");
+console.error("Hola mundo");
+
+// mostrar algo en el html
+document.writeln("Hola mundo");
+document.writeln("<h1 class='h1-color'>Hola mundo</h1>");
+
+// ventanas emergentes
+// alert("Practica de alert")
+
